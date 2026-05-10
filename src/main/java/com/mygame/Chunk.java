dc2c8a6793f2f254e8ca6eb661fb187eb40414ae
Seif I. Attia <seifislam871@gmail.com>
2026-05-10 16:12:56 +0300

    Revert "AAAtrying"

--- src/main/java/com/mygame/Chunk.java
+++ src/main/java/com/mygame/Chunk.java
@@ -10,48 +10,48 @@ package com.mygame;
  */
 public class Chunk {
 
-    public static final int CHUNK_SIZE = 16;
-    public static final int CHUNK_HEIGHT = 128;
+        public static final int CHUNK_SIZE = 16;
+        public static final int CHUNK_HEIGHT = 128;
 
-    // blocks 
-    /*
+        // blocks 
+        /*
                 air ->                      0
                 dirt  ->                   1
                 grass->                 2
                 bedrock->          3
-     */
-    private byte[][][] blocks;
+         */
+        private byte[][][] blocks;
 
-    public Chunk() {
-        blocks = new byte[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE];
-        generateSuperFlat();
+        public Chunk() {
+                blocks = new byte[CHUNK_SIZE][CHUNK_HEIGHT][CHUNK_SIZE];
+                generateSuperFlat();
 
-    }
+        }
 
-    private void generateSuperFlat() {
-        for (int x = 0; x < CHUNK_SIZE; x++) {
-            for (int z = 0; z < CHUNK_SIZE; z++) {
-                for (int y = 0; y < CHUNK_HEIGHT; y++) {
-                    if (y == 0) {
-                        blocks[x][y][z] = 3;     // bedrock
-                    } else if (y < 5) {
-                        blocks[x][y][z] = 1;     // dirt
-                    } else if (y == 5) {
-                        blocks[x][y][z] = 2;     //grass
-                    } else {
-                        blocks[x][y][z] = 0;    //air
-                    }
+        private void generateSuperFlat() {
+                for (int x = 0; x < CHUNK_SIZE; x++) {
+                        for (int z = 0; z < CHUNK_SIZE; z++) {
+                                for (int y = 0; y < CHUNK_HEIGHT; y++) {
+                                        if (y == 0) {
+                                                blocks[x][y][z] = 3;     // bedrock
+                                        } else if (y < 5) {
+                                                blocks[x][y][z] = 1;     // dirt
+                                        } else if (y == 5) {
+                                                blocks[x][y][z] = 2;     //grass
+                                        } else {
+                                                blocks[x][y][z] = 0;    //air
+                                        }
 
+                                }
+                        }
                 }
-            }
         }
-    }
 
-    public byte getBlock(int x, int y, int z) {
-        if (x < 0 || x >= CHUNK_SIZE || y < 0 || y >= CHUNK_HEIGHT || z < 0 || z >= CHUNK_SIZE) {
-            return 0; // Treat out of bounds as Air for now
+        public byte getBlock(int x, int y, int z) {
+                if (x < 0 || x >= CHUNK_SIZE || y < 0 || y >= CHUNK_HEIGHT || z < 0 || z >= CHUNK_SIZE) {
+                        return 0; // Treat out of bounds as Air for now
+                }
+                return blocks[x][y][z];
         }
-        return blocks[x][y][z];
-    }
 
 }
--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -7,7 +7,6 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
 import com.jme3.scene.shape.Box;
-import com.jme3.system.AppSettings;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
@@ -17,53 +16,42 @@ import com.jme3.system.AppSettings;
  */
 public class Main extends SimpleApplication {
 
-    public static void main(String[] args) {
-        Main app = new Main();
-
-        AppSettings settings = new AppSettings(true);
-        settings.setTitle("MineKraft");
-
-        // 4x MSAA
-        settings.setSamples(4);
-
-        settings.setResolution(1280, 768);
-        settings.setSamples(16);
-        settings.setVSync(false);
-        settings.setFrameRate(-1);
-        settings.setBitsPerPixel(32);
-        settings.setGammaCorrection(false);
-
-        app.setSettings(settings);
-        app.setShowSettings(false);
-        app.start();
-    }
-
-    private WorldManager myWorld;
-
-    @Override
-    public void simpleInitApp() {
-        myWorld = new WorldManager(this, rootNode, assetManager);
-
-        cam.setLocation(new com.jme3.math.Vector3f(-10, 50, -10));
-        cam.lookAt(new com.jme3.math.Vector3f(24, 0, 24), com.jme3.math.Vector3f.UNIT_Y);
-        flyCam.setMoveSpeed(70f);
-    }
-
-    @Override
-    public void simpleUpdate(float tpf) {
-        myWorld.update(cam.getLocation());
-    }
-
-    @Override
-    public void simpleRender(RenderManager rm) {
-        //TODO: add render code
-    }
-
-    @Override
-    public void destroy() {
-        if (myWorld != null) {
-            myWorld.destroy();
+        public static void main(String[] args) {
+                Main app = new Main();
+                app.start();
         }
-        super.destroy(); // for jME clean-up
-    }
+
+        @Override
+        public void simpleInitApp() {
+                Chunk myFirstChunk = new Chunk();
+                ChunkMesher mesher = new ChunkMesher();
+
+                Mesh chunkMesh = mesher.createMesh(myFirstChunk);
+
+                Geometry chunkGeo = new Geometry("ChunkGeo", chunkMesh);
+                Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+
+                mat.getAdditionalRenderState().setWireframe(true);
+
+                mat.setColor("Color", ColorRGBA.Green);
+                chunkGeo.setMaterial(mat);
+
+                rootNode.attachChild(chunkGeo);
+                // 3. Move the Camera back and up to see the whole chunk
+                cam.setLocation(new com.jme3.math.Vector3f(-10, 20, -10));
+                cam.lookAt(new com.jme3.math.Vector3f(8, 0, 8), com.jme3.math.Vector3f.UNIT_Y);
+
+                // Optional: Speed up the fly camera so you can move around faster
+                flyCam.setMoveSpeed(30f);
+        }
+
+        @Override
+        public void simpleUpdate(float tpf) {
+                //TODO: add update code
+        }
+
+        @Override
+        public void simpleRender(RenderManager rm) {
+                //TODO: add render code
+        }
 }

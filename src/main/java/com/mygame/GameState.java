080d4ff748249d659a2d8e19ad907c4ee3e16504
Bored_AH <eyadalaaeldein@gmail.com>
2026-05-10 18:25:01 +0300

    Early version of main menu, added game states

---
+++ src/main/java/com/mygame/GameState.java
@@ -0,0 +1,58 @@
+package com.mygame;
+
+import com.jme3.app.Application;
+import com.jme3.app.SimpleApplication;
+import com.jme3.app.state.BaseAppState;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
+import com.jme3.scene.Node;
+
+public class GameState extends BaseAppState {
+
+    private SimpleApplication app;
+    private WorldManager myWorld;
+    private Camera cam;
+    private Node rootNode;
+
+    @Override
+    protected void initialize(Application app) {
+        // Cast to SimpleApplication to access rootNode, assetManager, etc.
+        this.app = (SimpleApplication) app;
+        this.cam = this.app.getCamera();
+        this.rootNode = this.app.getRootNode();
+
+        // Initialize the world logic moved from Main.simpleInitApp
+        myWorld = new WorldManager(this.app, rootNode, this.app.getAssetManager());
+
+        // Setup camera
+        cam.setLocation(new Vector3f(-10, 50, -10));
+        cam.lookAt(new Vector3f(24, 0, 24), Vector3f.UNIT_Y);
+        this.app.getFlyByCamera().setMoveSpeed(70f);
+    }
+
+    @Override
+    public void update(float tpf) {
+        // The game loop logic moved from Main.simpleUpdate
+        if (myWorld != null) {
+            myWorld.update(cam.getLocation());
+        }
+    }
+
+    @Override
+    protected void cleanup(Application app) {
+        // Clean up the world when this state is detached
+        if (myWorld != null) {
+            myWorld.destroy();
+        }
+    }
+
+    @Override
+    protected void onEnable() {
+        // Logic for when the game is unpaused or shown
+    }
+
+    @Override
+    protected void onDisable() {
+        // Logic for when the game is paused or hidden
+    }
+}
--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -1,20 +1,9 @@
 package com.mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.Mesh;
-import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
 
-/**
- * This is the Main Class of your Game. You should only do initialization here.
- * Move your Logic into AppStates or Controls
- *
- * @author normenhansen
- */
 public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
@@ -22,12 +11,8 @@ public class Main extends SimpleApplication {
 
         AppSettings settings = new AppSettings(true);
         settings.setTitle("MineKraft");
-
-        // 4x MSAA
-        settings.setSamples(4);
-
-        settings.setResolution(1280, 768);
         settings.setSamples(16);
+        settings.setResolution(1280, 768);
         settings.setVSync(false);
         settings.setFrameRate(-1);
         settings.setBitsPerPixel(32);
@@ -38,32 +23,24 @@ public class Main extends SimpleApplication {
         app.start();
     }
 
-    private WorldManager myWorld;
-
     @Override
     public void simpleInitApp() {
-        myWorld = new WorldManager(this, rootNode, assetManager);
-
-        cam.setLocation(new com.jme3.math.Vector3f(-10, 50, -10));
-        cam.lookAt(new com.jme3.math.Vector3f(24, 0, 24), com.jme3.math.Vector3f.UNIT_Y);
-        flyCam.setMoveSpeed(70f);
+        // For now, we jump straight into the GameState. 
+        // Later, you will attach MenuState here instead.
+        
+        setDisplayFps(false);
+        setDisplayStatView(false);
+        
+        stateManager.attach(new MenuState());
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        myWorld.update(cam.getLocation());
+        // Keep this empty; logic is now in GameState.update()
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
-        //TODO: add render code
-    }
-
-    @Override
-    public void destroy() {
-        if (myWorld != null) {
-            myWorld.destroy();
-        }
-        super.destroy(); // for jME clean-up
+        // Custom rendering code if needed
     }
 }

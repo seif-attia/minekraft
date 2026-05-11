75ea17edc2410f2705c58fc09c50b62b551e6d1a
Bored_AH <eyadalaaeldein@gmail.com>
2026-05-12 02:46:16 +0300

    Added app and window icon

--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -3,6 +3,8 @@ package com.mygame;
 import com.jme3.app.SimpleApplication;
 import com.jme3.renderer.RenderManager;
 import com.jme3.system.AppSettings;
+import java.io.File;
+import java.io.IOException;
 
 public class Main extends SimpleApplication {
 
@@ -12,6 +14,15 @@ public class Main extends SimpleApplication {
         AppSettings settings = new AppSettings(true);
         settings.setTitle("MineKraft");
         settings.setSamples(16);
+        try {
+        // This looks for your icon in the assets folder
+        // Use a 256x256 PNG for the best result
+        settings.setIcons(new java.awt.image.BufferedImage[]{
+            javax.imageio.ImageIO.read(new File("assets/Textures/App Icon 1.png"))
+        });
+        } catch (IOException e) {
+            System.err.println("Could not load window icon: " + e.getMessage());
+        }
         settings.setResolution(1280, 768);
 //        settings.setResolution(1920, 1080);
 //        settings.setFullscreen(true);
